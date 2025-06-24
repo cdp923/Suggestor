35895fffc688ca775d9529e2e9a69de0e1dfed1f
cdp923 <158327739+cdp923@users.noreply.github.com>
2025-06-23 23:02:16 -0400

    Keyboard and dictionary are initilized.

---
+++ keyboardPrep.cpp
@@ -0,0 +1,64 @@
+#include "keyboardPrep.h"
+
+#include <cstdio>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+int keyNumber = 26; 
+std::vector<std::vector<char>> keyGraph;
+std::vector<std::vector<std::string>> dictGraph(26);
+std::vector<std::vector<char>> initKeyboard(){
+    std::vector<std::vector<char>> keyGraphInitilizer;
+    std::vector<char> q = {'a','w'};
+    std::vector<char> w = {'q','a','s','d','e'};
+    std::vector<char> e = {'w','s','d','r'};
+    std::vector<char> r = {'e','d','f','t'};
+    std::vector<char> t = {'r','f','g','h','y'};
+    std::vector<char> y = {'t','g','j','u'};
+    std::vector<char> u = {'y','h','j','i'};
+    std::vector<char> i = {'u','j','k','o'};
+    std::vector<char> o = {'i','k','l','p'};
+    std::vector<char> p = {'o','l'};
+    std::vector<char> a = {'q','w','s','z'};
+    std::vector<char> s = {'w','a','z','x','d','e'};
+    std::vector<char> d = {'e','s','x','c','f','r'};
+    std::vector<char> f = {'r','d','c','v','g','t'};
+    std::vector<char> g = {'t','f','v','b','h','y'};
+    std::vector<char> h = {'y','g','b','n','j','u'};
+    std::vector<char> j = {'u','h','n','m','k','i'};
+    std::vector<char> k = {'i','j','m','l','o'};
+    std::vector<char> l = {'o','k','p'};
+    std::vector<char> z = {'a','s','x'};
+    std::vector<char> x = {'z','s','d','c'};
+    std::vector<char> c = {'x','d','f','v'};
+    std::vector<char> v = {'c','f','g','b'};
+    std::vector<char> b = {'v','g','h','n'};
+    std::vector<char> n = {'b','h','j','m'};
+    std::vector<char> m = {'n','j','k'};
+    keyGraphInitilizer = {q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m};
+    return keyGraphInitilizer;
+} 
+std::vector<std::vector<std::string>> initDictionary(){
+    std::ifstream file("Dictionary.txt");
+    if (!file.is_open()) {
+        std::cerr << "Error: Could not open file." << std::endl;
+    }
+    std::string word;
+    int counter = 0;
+    char firstLetter = 'a';
+    while (std::getline(file, word))
+    {
+        if (word.at(0) != firstLetter){
+            counter++;
+            firstLetter++;
+            printf("\n");
+        }
+        dictGraph[counter].push_back(word);
+        printf("%s,", word.c_str());
+    }  
+    return dictGraph;
+}
+int main(){
+    initDictionary();
+    return 0;
+}
---
+++ keyboardPrep.h
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+extern int keyNumber; 
+extern std::vector<std::vector<char>> keyGraph;
+extern std::vector<std::vector<std::string>> dictGraph;
+
+
+std::vector<std::vector<char>> initKeyboard();
+std::vector<std::vector<std::string>> initDictionary();
